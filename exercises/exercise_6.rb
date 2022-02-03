3339ac20b75e4e34099bde7bc80bd4b89978e6ab
Selene Grossett <selene_816@hotmail.com>
2022-02-03 01:26:04 +0000

    Create employes for the employes table.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Penny", last_name: "Proud", hourly_rate: 100)
+@store2.employees.create(first_name: "King", last_name: "Bob", hourly_rate: 50)
+@store1.employees.create(first_name: "Randall", last_name: "Weems", hourly_rate: 10)
+@store2.employees.create(first_name: "Kim", last_name: "Possible", hourly_rate: 60)
+@store1.employees.create(first_name: "Ron", last_name: "Stopable", hourly_rate: 90)
+@store2.employees.create(first_name: "Avatar", last_name: "Aang", hourly_rate: 100)
+@store1.employees.create(first_name: "Cornelius", last_name: "Fillmore", hourly_rate: 90)
+@store2.employees.create(first_name: "Tino", last_name: "Tonitini", hourly_rate: 70)
+@store1.employees.create(first_name: "Carver", last_name: "Descartes", hourly_rate: 80)
+@store2.employees.create(first_name: "Ashley", last_name: "Spinelli", hourly_rate: 90)
